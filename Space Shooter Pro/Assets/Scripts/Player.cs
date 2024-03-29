03f84720cd994cc0e03ee6d29f8632a11f8635ff
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2024-03-29 12:21:14 -0400

    Updates

--- Space Shooter Pro/Assets/Scripts/Player.cs
+++ Space Shooter Pro/Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private float speed = 3.5f;
 
+    public float inputHorizontal;
+    public float inputVertical;
+
     // Start is called before the first frame update
     void Start() {
 
@@ -18,7 +21,31 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        //transform.Translate(Vector3.right * Time.deltaTime * speed);
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+
+        //transform.Translate(Vector3.right * inputHorizontal * Time.deltaTime * speed);
+        //transform.Translate(Vector3.up * inputVertical * Time.deltaTime * speed);
+        transform.Translate(new Vector3(inputHorizontal, inputVertical, 0) * Time.deltaTime * speed);
+
+        if (transform.position.x > 10) {
+
+            transform.position = new Vector3(-10, transform.position.y, 0);
+
+        } else if (transform.position.x < -10) {
+
+            transform.position = new Vector3(10, transform.position.y, 0);
+        }
+
+        if (transform.position.y > 5.5f) {
+
+            transform.position = new Vector3(transform.position.x, 5.5f, 0);
+        } else if (transform.position.y < -3.5f) {
+
+            transform.position = new Vector3(transform.position.x, -3.5f, 0);
+        }
+
 
     }
 }
