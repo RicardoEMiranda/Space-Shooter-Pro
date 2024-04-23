2e0d44715c2093cd7090043ba7525a14d81b2c88
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2024-04-23 13:15:58 -0400

    Tripple Shot Powerup Behavior
    Created Tripple Shot Powerup.
    Created Powerup object and added 2D Box Collider (isTrigger) and Rigidbody (0 gravity).
    Created PowerUp_Trippleshot.cs with speed property.
    Created logic so powerup destroys itself once off screen.
    Also destroys self if collides with Player, using OnTriggerEnter2D and checking the other.tag == "Player"

---
+++ Space Shooter Pro/Assets/Scripts/Powerup_TrippleShot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_TrippleShot : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()  {
+
+
+        //move power up at speed 3
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if(transform.position.y<=-6.5) {
+            Destroy(this.gameObject);
+        }
+
+        //detect collision with player and destroy self after collision
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if(other.tag == "Player") {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
