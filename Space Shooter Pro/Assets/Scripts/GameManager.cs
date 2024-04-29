fe27e5303d1783b436b240a72279336601cb1999
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2024-04-28 20:04:45 -0400

    Main Menu Design
    Imported Sci-Fi Buttons & Panels Package.
    Designed Main Menu UI along with interactable Start button.
    Created a MainMenu.cs inside of a MainMenu object in the hierarchy.
    Created a transparent button on top of the Rocket logo at the start menu.
    The MainMenu calls the OnClick event (StartGame()) when the transparent button is clicked.
    When the Start button is clicked, the StartGame() method calls SceneManager.LoadScene("GameScene") to start the game.

--- Space Shooter Pro/Assets/Scripts/GameManager.cs
+++ Space Shooter Pro/Assets/Scripts/GameManager.cs
@@ -5,15 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-
     // Start is called before the first frame update
-    void Start()  {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()  {
-        
+    void Update() {
+
     }
 
     public void RestartButtonClicked() {
