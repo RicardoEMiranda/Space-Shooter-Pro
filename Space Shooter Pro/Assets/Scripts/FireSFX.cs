634e3187700bad8fbeedae583fb3456975e2bf20
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2024-05-02 19:07:29 -0400

    Fire Audio Source
    Added an Audio Source to the Fire prefab. When the player gets hit by an enemy, when the Fire prefab turns active, will play a Fire SFX.
    
    Created a FireSFX script and attached it to the Fire prefab. The Script gets a reference to the Fire SFX audio Clip and plays it on the Fire Prefab's Audio Source.
    
    At Start (when the prefab is activated by the player's health), set audioSource to enabled to turn it on since it's set to inactive as its default state. Then set the audio source clip to audioClip_Fire and then Play the audio clip.

---
+++ Space Shooter Pro/Assets/Scripts/FireSFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSFX : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip audioClip_Fire;
+
+    private bool audioSourceActive;
+
+    // Start is called before the first frame update
+    void Start() {
+        audioSource.enabled = true;
+        audioSource.clip = audioClip_Fire;
+        audioSource.Play();
+    }
+
+}
