82b0dfcc353a45175c1484e2fc8009b58a5ebb30
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2024-05-04 11:24:05 -0400

    Pause Game & Exit Features
    Wrote the logic for ExitButtonClicked( ) and ResumeButtonClicked( ) methods inside of GameManager.cs. Handles both cases when player clicks either Resume or Exit after having paused the game.

--- Space Shooter Pro/Assets/Scripts/GameManager.cs
+++ Space Shooter Pro/Assets/Scripts/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : MonoBehaviour {
     [SerializeField]
     private GameObject pauseModal;
 
-
-
     // Start is called before the first frame update
     void Start() {
 
@@ -28,13 +26,27 @@ public class GameManager : MonoBehaviour {
     public void PauseButtonClicked() {
         Debug.Log("Pause Button clicked");
         pauseModal.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void ResumeButtonClicked() {
         Debug.Log("Resume Button clicked");
+        pauseModal.SetActive(false);
+        Time.timeScale = 1;
     }
 
     public void ExitButtonClicked() {
         Debug.Log("Exit Button clicked");
+
+        //Preprocessor Directives. Conitionally compiles code depending on the condition.
+        //If game is playing in the Unity Editor, then will use UnityEditor.EditorApplication.isPlaying to quit the application,
+        //since Application.Quit() doesn't work when running on the editor. Otherwise, if playing from the Windows build,
+        ////uses Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        # endif 
+        Application.Quit();
     }
 }
